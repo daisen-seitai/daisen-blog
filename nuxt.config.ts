0ddbaf9a78d744fe6388b9d909f101e5af8e49b3
daisen-seitai <admin@daisen-seitai.com>
2026-04-01 18:18:40 +0900

    simplify route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   routeRules: {
-    '/': { prerender: true },
-    '/post/**': { prerender: false },
     '/admin': { ssr: false },
   }
 })
