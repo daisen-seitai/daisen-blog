481fa8839cfe5edcf31179c72aa37d3bb92c04f6
daisen-seitai <admin@daisen-seitai.com>
2026-04-01 18:13:56 +0900

    fix route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  routeRules: {
+    '/': { prerender: true },
+    '/post/**': { prerender: false },
+    '/admin': { ssr: false },
+  }
 })
