a4d57c0b1649da600b3ca4100504e135ee34e76e
daisen-seitai <admin@daisen-seitai.com>
2026-04-15 21:04:27 +0900

    fix: baseURL / に変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  srcDir: 'app/',
+  ssr: false,
+  app: {
+    baseURL: '/',
+  },
   routeRules: {
     '/admin': { ssr: false },
   },
